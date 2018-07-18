c72275c48f07b26025850e15ea467f8f45851566
lisiyu <18392955246@163.com>
2018-07-18 16:10:09 +0800

    修改

---
+++ test1/src/main/java/com/hand/f1.java
@@ -0,0 +1,7 @@
+package com.hand;
+
+public class f1 {
+    public static void main(String[] args) {
+        System.out.printf("f1----------");
+    }
+}
---
+++ test2/src/main/java/com/hand/f2.java
@@ -0,0 +1,7 @@
+package com.hand;
+
+public class f2 {
+    public static void main(String[] args) {
+        System.out.printf("f2----------");
+    }
+}
